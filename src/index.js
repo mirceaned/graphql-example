159fcaba4429612a08d1fc2ac20841535ce5bdd0
Mircea Nedelcu <mnedelcu@cj.com>
2019-12-04 23:16:03 -0800

    add simple server for users

---
+++ src/index.js
@@ -0,0 +1,49 @@
+
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+    type Query {
+        users: [User!]!
+        user(id: ID!): User
+    }
+    
+    type User {
+        id: ID!
+        name: String
+        birthDate: String
+        username: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        users() {
+            return users;
+        },
+        user(_, args) {
+            return users.find(user => user.id === args.id);
+        }
+    },
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => console.log("Server is running on http://localhost:4000"));
+
+const users = [
+    {
+        id: "1",
+        name: "Ada Lovelace",
+        birthDate: "1815-12-10",
+        username: "@ada"
+    },
+    {
+        id: "2",
+        name: "Alan Turing",
+        birthDate: "1912-06-23",
+        username: "@complete"
+    }
+];
